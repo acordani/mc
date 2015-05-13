4cdca2a4454bdf15244450a1e14a8d68e4b8c2d5
Alexandre Cordani <alcordani@gmail.com>
2015-05-13 16:02:52 +0200

    controller cocktail-new, view cocktail new

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,14 @@
+class CocktailsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
   resources :cocktails, only: [:index,:new, :show, :create] do
     resources :doses, only: [:new, :create]
   end
